e16ae5a382a5f108ac7a12467654d12962133df1
bobyrev-ilya <bobyrev-1997@mail.ru>
2021-10-01 02:32:34 +0300

    Реализация 14 дз в многопоточном режиме

--- src/main/java/com/sbrf/reboot/bank/MainReport.java
+++ src/main/java/com/sbrf/reboot/bank/MainReport.java
@@ -1,14 +1,17 @@
 package com.sbrf.reboot.bank;
 
 import lombok.SneakyThrows;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MainReport {
@@ -19,58 +22,66 @@ public class MainReport {
     public static final LocalDate END_DATE = LocalDate.of(2021, Month.AUGUST, 1);
 
 
+    @SneakyThrows
     public static void main(String[] args) {
         Account account1 = new Account(BigDecimal.valueOf(20), "RUB", LocalDate.of(2021, Month.JULY, 2));
         Account account2 = new Account(BigDecimal.valueOf(206), "EUR", LocalDate.of(2021, Month.JULY, 1));
         Account account3 = new Account(BigDecimal.valueOf(13), "RUB", LocalDate.of(2021, Month.AUGUST, 3));
         Account account4 = new Account(BigDecimal.valueOf(1.06), "RUB", LocalDate.of(2021, Month.JULY, 26));
 
-        Customer customer1 = new Customer(20, "Ivan", new ArrayList<Account>() {{
-            add(account1);
-            add(account2);
-        }});
+        Customer customer1 = new Customer(20, "Ivan", Arrays.asList(account1, account2));
 
-        Customer customer2 = new Customer(31, "Egor", new ArrayList<Account>() {{
-            add(account3);
-            add(account4);
-        }});
+        Customer customer2 = new Customer(31, "Egor", Arrays.asList(account3, account4));
 
-        Customer customer3 = new Customer(18, "Katy", new ArrayList<Account>() {{
-            add(account1);
-            add(account4);
-        }});
+        Customer customer3 = new Customer(18, "Katy", Arrays.asList(account1, account4));
 
-        BigDecimal sum = MainReport.getTotalsWithCompletableFuture(Stream.of(customer1, customer2, customer3));
-        BigDecimal sum1 = MainReport.getTotalsWithReact(Stream.of(customer1, customer2, customer3));
+        System.out.println("Start processing ...");
+        getTotalsWithReact(Stream.of(customer1, customer2, customer3));
+        getTotalsWithCompletableFuture(Stream.of(customer1, customer2, customer3));
+        System.out.println("Start sleeping ...");
+        Thread.sleep(5000);
+        System.out.println("Stop sleeping ...");
     }
 
     @SneakyThrows
-    public static BigDecimal getTotalsWithCompletableFuture(Stream<Customer> customerStream) {
-        return CompletableFuture.supplyAsync(() -> customerStream
-                                .filter(c -> c.getAge() >= MIN_AGE && c.getAge() <= MAX_AGE)
-                                .flatMap(Customer::getAccounts)
+    public static void getTotalsWithCompletableFuture(Stream<Customer> customerStream) {
+
+        //коллекция Future для каждого Account
+        List<CompletableFuture<BigDecimal>> completableFutureList = customerStream
+                .filter(c -> c.getAge() >= MIN_AGE && c.getAge() <= MAX_AGE)
+                .map(Customer::getAccounts)
+                .map(x -> CompletableFuture.supplyAsync(
+                        () -> x
                                 .filter(a -> START_DATE.isBefore(a.getCreateDate()) || START_DATE.isEqual(a.getCreateDate()))
                                 .filter(a -> END_DATE.isAfter(a.getCreateDate()) || END_DATE.isEqual(a.getCreateDate()))
                                 .filter(a -> "RUB".equals(a.getCurrency()))
                                 .map(Account::getAmount)
-                                .reduce(BigDecimal::add)
-                                .orElse(BigDecimal.valueOf(0)),
-                        Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()))
-                .get();
+                                .reduce(BigDecimal.valueOf(0), BigDecimal::add),
+                        Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())
+                ))
+                .collect(Collectors.toList());
+
+        //объединяем все Future в один, ждем выполнения и получаем результаты каждого CompletableFuture из ранее собранной коллекции
+        //блокировки из-за join не так как объединенный CompletableFuture выполнился по thenApply
+        CompletableFuture
+                .allOf(completableFutureList.toArray(new CompletableFuture[0]))
+                .thenApply(x -> completableFutureList.stream()
+                        .map(CompletableFuture::join)
+                        .reduce(BigDecimal.ZERO, BigDecimal::add))
+                .thenAccept(System.out::println);
+
     }
 
-    public static BigDecimal getTotalsWithReact(Stream<Customer> customerStream) {
-        Mono<BigDecimal> mono = Mono.just(
-                customerStream.filter(c -> c.getAge() >= MIN_AGE && c.getAge() <= MAX_AGE)
-                        .flatMap(Customer::getAccounts)
+    public static void getTotalsWithReact(Stream<Customer> customerStream) {
+        customerStream.filter(c -> c.getAge() >= MIN_AGE && c.getAge() <= MAX_AGE)
+                .map(Customer::getAccounts)
+                .map(x -> Flux.fromStream(x).publishOn(Schedulers.newParallel("Test")).log()
                         .filter(a -> START_DATE.isBefore(a.getCreateDate()) || START_DATE.isEqual(a.getCreateDate()))
                         .filter(a -> END_DATE.isAfter(a.getCreateDate()) || END_DATE.isEqual(a.getCreateDate()))
                         .filter(a -> "RUB".equals(a.getCurrency()))
-                        .map(Account::getAmount)
-                        .reduce(BigDecimal::add)
-                        .orElse(BigDecimal.valueOf(0))
-
-        );
-        return mono.block();
+                        .map(Account::getAmount))
+                .reduce(Flux.empty(), Flux::merge)
+                .reduce(BigDecimal.valueOf(0), BigDecimal::add)
+                .subscribe(System.out::println);
     }
 }
